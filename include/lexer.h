0d16be295f194c8cb7cbce984e488532cb376afd
Nicolai Brand <nicolahb@stud.ntnu.no>
2023-06-04 18:12:56 +0200

    basic lexing

--- include/lexer.h
+++ include/lexer.h
@@ -5,54 +5,71 @@
 #define NICC_DARR_IMPLEMENTATION
 #include "nicc/nicc.h"
 
+#define STATE_FN(___state_fn) \
+    (StateFn)                 \
+    {                         \
+	.fn = ___state_fn     \
+    }
+
 typedef enum {
     /* keywords */
-    t_var,
+    t_var = 0,
     t_if,
-    t_elif,
-    t_else,
-    t_loop,
-    t_in,
+    // t_elif,
+    // t_else,
+    // t_loop,
+    // t_in,
     t_true,
     t_false,
-    t_as,
-    t_and,
-    t_or,
+    // t_as,
+    // t_and,
+    // t_or,
+
     t_str,
-    t_num,
-    t_bool,
+    // t_num,
+    // t_bool,
 
     /* single-character tokens */
-    t_lparen,
-    t_rparen,
+    // t_lparen,
+    // t_rparen,
     t_lbrace,
     t_rbrace,
-    t_star,
-    t_dollar,
+    // t_star,
+    // t_dollar,
+    t_newline,
 
     /* one or two character tokens */
-    t_anp,
-    // ...
+    // t_anp,
+    //  ...
 
     /* data types */
-    dt_str,
-    dt_num,
-    dt_range,
-    dt_bool,
-    dt_word,
+    // dt_str,
+    // dt_num,
+    // dt_range,
+    // dt_bool,
+    // dt_shlit,
 
     t_identifier,
-    t_error,
     t_eof,
+    t_error,
     t_enum_count,
 } TokenType;
 
+extern char *keywords[t_false + 1];
+
+/* arena string */
+typedef struct {
+    char *p;
+    size_t size;
+} SlashStr;
+
 typedef struct {
     TokenType type;
-    union {
-	char *lexeme;
-	char *error;
-    };
+    SlashStr lexeme;
+    // union {
+    //     char *lexeme;
+    //     char *error;
+    // };
 } Token;
 
 typedef struct {
--- src/lexer.c
+++ src/lexer.c
@@ -14,27 +14,34 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include "lexer.h"
 #include <stdio.h>
 #include <string.h>
 
-// temporary as f
-static char *str_slice(char *s, size_t start, size_t end)
+#include "lexer.h"
+#define SAC_IMPLEMENTATION
+#define SAC_TYPEDEF
+#include "sac/sac.h"
+
+char *keywords[t_false + 1] = {
+    "var",
+    "if",
+    "true",
+    "false",
+};
+
+void slash_str_println(SlashStr s)
 {
-    size_t len = end - start + 2;
-    char *str = malloc(sizeof(char) * len);
-
-    memcpy(str, s + start, len - 1);
-    str[len - 1] = 0;
-
-    return str;
+    char str[s.size + 1];
+    memcpy(str, s.p, s.size);
+    str[s.size] = 0;
+    printf("'%s'\n", str);
 }
 
+void emit(Lexer *lexer, TokenType type);
 
 /*
  * helper functions
  */
-
 static char next(Lexer *lexer)
 {
     if (lexer->input[lexer->pos] == 0)
@@ -81,7 +88,7 @@ static bool consume(Lexer *lexer, char *accept)
     return false;
 }
 
-static void consumeRun(Lexer *lexer, char *accept)
+static void consume_run(Lexer *lexer, char *accept)
 {
     while (1) {
 	if (!consume(lexer, accept))
@@ -89,40 +96,136 @@ static void consumeRun(Lexer *lexer, char *accept)
     }
 }
 
+/*
+ * semantic
+ */
+bool is_space(char c)
+{
+    return c == ' ' || c == '\t' || c == '\v';
+}
+
+bool is_numeric(char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+bool is_alpha(char c)
+{
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+bool is_valid_identifier(char c)
+{
+    return is_numeric(c) || is_alpha(c) || c == '_';
+}
+
 
 /*
  * state functions
  */
-
-static void *lex_error(Lexer *lexer, char *err_str)
-{
-    Token *token = malloc(sizeof(Token));
-    token->type = t_error;
-    token->error = err_str;
-    darr_append(lexer->tokens, token);
-    return NULL;
-}
+StateFn lex_any(Lexer *lexer);
+StateFn lex_end(Lexer *lexer);
+StateFn lex_number(Lexer *lexer);
+StateFn lex_identifier(Lexer *lexer);
+StateFn lex_string(Lexer *lexer);
 
 StateFn lex_any(Lexer *lexer)
 {
+    while (1) {
+	char c = next(lexer);
+	switch (c) {
+	case ' ':
+	case '\t':
+	    ignore(lexer);
+	    break;
+
+	case '\n':
+	    emit(lexer, t_newline);
+	    break;
+
+	// TODO: lex_block state function
+	case '{':
+	    emit(lexer, t_lbrace);
+	    break;
+	case '}':
+	    emit(lexer, t_rbrace);
+	    break;
+
+	case EOF:
+	    return STATE_FN(lex_end);
+
+	case '"':
+	    return STATE_FN(lex_string);
+
+	default:
+	    // if (is_alpha(c)) {
+	    //     backup(lexer);
+	    //     return STATE_FN(lex_number);
+	    // }
+
+	    if (is_valid_identifier(c)) {
+		backup(lexer);
+		return STATE_FN(lex_identifier);
+	    }
+
+	    goto error;
+	}
+    }
+
+error:
+    printf("error\n");
+    return STATE_FN(NULL);
+}
+
+StateFn lex_end(Lexer *lexer)
+{
+    emit(lexer, t_eof);
     return (StateFn){ .fn = NULL };
 }
 
 StateFn lex_number(Lexer *lexer);
 
-StateFn lex_identifier(Lexer *lexer);
+StateFn lex_identifier(Lexer *lexer)
+{
+    while (is_valid_identifier(next(lexer)))
+	;
+    backup(lexer);
+    // TODO resolve what identifier it is
+    emit(lexer, t_identifier);
+    return STATE_FN(lex_any);
+}
 
-StateFn lex_string(Lexer *lexer);
+StateFn lex_string(Lexer *lexer)
+{
+    /* ignore starting qoute */
+    ignore(lexer);
+
+    char c;
+    while ((c = next(lexer)) != '"') {
+	if (c == EOF)
+	    return STATE_FN(NULL);
+    }
+
+    /* backup final qoute */
+    backup(lexer);
+    emit(lexer, t_str);
+    /* advance past final qoute and ignore it */
+    next(lexer);
+    ignore(lexer);
+
+    return STATE_FN(lex_any);
+}
+
 
 /*
  * fsm
  */
-
 void emit(Lexer *lexer, TokenType type)
 {
     Token *token = malloc(sizeof(Token));
     token->type = type;
-    token->lexeme = str_slice(lexer->input, lexer->start, lexer->pos);
+    token->lexeme =
+	(SlashStr){ .p = lexer->input + lexer->start, .size = lexer->pos - lexer->start };
     darr_append(lexer->tokens, token);
 
     lexer->start = lexer->pos;
@@ -134,11 +237,8 @@ void run(Lexer *lexer)
     for (StateFn state = start_state; state.fn != NULL;) {
 	state = state.fn(lexer);
     }
-
-    // https://www.youtube.com/watch?v=HxaD_trXwRE
 }
 
-// list<Token> *lex(char *input)
 struct darr_t *lex(char *input)
 {
     struct darr_t *tokens = darr_malloc();
@@ -149,9 +249,35 @@ struct darr_t *lex(char *input)
 
 int main(void)
 {
-    char *input = "if true { echo \"Hello\" }";
-    struct darr_t *tokens = lex(input);
-    for (size_t i = 0; i < tokens->size; i++) {
-	printf("%s\n", ((Token *)darr_get(tokens, i))->lexeme);
+    // https://www.youtube.com/watch?v=HxaD_trXwRE
+    char input[1024];
+    FILE *fp = fopen("src/test.sh", "r");
+    if (fp == NULL) {
+	return -1;
     }
+
+    int c;
+    size_t i = 0;
+    do {
+	c = fgetc(fp);
+	input[i++] = c;
+	if (i == 1024)
+	    break;
+    } while (c != EOF);
+
+    input[--i] = 0;
+
+
+    // Arena lex_arena;
+    // m_arena_init_dynamic(&lex_arena, SAC_DEFAULT_CAPACITY, SAC_DEFAULT_COMMIT_SIZE);
+
+    struct darr_t *tokens = lex(input);
+    for (i = 0; i < tokens->size; i++) {
+	// printf("%s\n", ((Token *)darr_get(tokens, i))->lexeme);
+	Token *token = darr_get(tokens, i);
+	printf("[%zu] ", i);
+	slash_str_println(token->lexeme);
+    }
+
+    // m_arena_release(&lex_arena);
 }
