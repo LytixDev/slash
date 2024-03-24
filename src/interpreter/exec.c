ecb549a6cf60e99d0e00ef1de77f168405bce7f8
Nicolai Brand <nicolahb@stud.ntnu.no>
2024-03-24 11:11:28 +0100

    update headers

--- src/interpreter/exec.c
+++ src/interpreter/exec.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2022-2023 Nicolai Brand, originally a part of the Valery project.
+ *  Copyright (C) 2022-2024 Nicolai Brand, originally a part of the Valery project.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- src/interpreter/value/cast.c
+++ src/interpreter/value/cast.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2023 Nicolai Brand (https://lytix.dev)
+ *  Copyright (C) 2023-2024 Nicolai Brand (https://lytix.dev)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
