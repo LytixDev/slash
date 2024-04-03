7c474e09b96ec9028134899e719beca758cdfa05
Nicolai Brand <nicolahb@stud.ntnu.no>
2024-04-03 21:20:36 +0200

    add __FILE__ and __LINE__ to runtime error on debug build

--- include/interpreter/error.h
+++ include/interpreter/error.h
@@ -34,6 +34,16 @@ extern jmp_buf runtime_error_jmp;
 void report_lex_err(Lexer *lexer, bool print_offending, char *msg);
 void report_parse_err(Parser *parser, char *msg);
 
+#ifdef DEBUG
+#define REPORT_RUNTIME_ERROR(...)                                                               \
+    do {                                                                                        \
+	REPORT_IMPL("%s[Slash Runtime Error @ %s:%d]:%s ", ANSI_BOLD_START, __FILE__, __LINE__, \
+		    ANSI_BOLD_END);                                                             \
+	REPORT_IMPL(__VA_ARGS__);                                                               \
+	REPORT_IMPL("\n");                                                                      \
+	longjmp(runtime_error_jmp, RUNTIME_ERROR);                                              \
+    } while (0);
+#else
 #define REPORT_RUNTIME_ERROR(...)                                                   \
     do {                                                                            \
 	REPORT_IMPL("%s[Slash Runtime Error]:%s ", ANSI_BOLD_START, ANSI_BOLD_END); \
@@ -41,5 +51,6 @@ void report_parse_err(Parser *parser, char *msg);
 	REPORT_IMPL("\n");                                                          \
 	longjmp(runtime_error_jmp, RUNTIME_ERROR);                                  \
     } while (0);
+#endif /* DEBUG */
 
 #endif /* ERROR_H */
